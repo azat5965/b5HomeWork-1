c735c574a0a384e1c37aa1dfee9f8a0d52c43eff
happylife979 <65377757+happylife979@users.noreply.github.com>
2020-05-14 21:59:12 -0400

    uploading my homework at 14may2020

--- FirstName.java
+++ FirstName.java
@@ -1,9 +1,10 @@
 public class FirstName {
 	
-	public static void main(String[] args){
-	      String name = "John";
-	      System.out.println(name);
-	      name = "David";
-	      System.out.println (name);
-	}
+    public static void main(String[] args){
+        String name = "Aiza";
+        System.out.println(name);
+        name = "David";
+        System.out.println(name);
+    }
+
 }
