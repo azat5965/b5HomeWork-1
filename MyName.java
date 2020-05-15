4d6890a4981fe2e99eee42111810e3e718afc787
AidanaM <65377422+AidanaM@users.noreply.github.com>
2020-05-14 22:02:04 -0400

    Aidana first HomeWork may142020.java

--- MyName.java
+++ MyName.java
@@ -1,16 +1,16 @@
 public class MyName {
+
+  public static void main(String[] args){
+    String firstName = "Aidana";
+    String lastName = "Melisova";
+
+
+    System.out.println("My first name is "+firstName+" and last name is "+lastName);
+
+
+  }
 	
-	public static void main(String[] args){
-	    String firstName = "Timur";
-	    String lastName = "Dzhigkaev";
 
-	    System.out.println("My first name is "+firstName+" and last name is "+lastName);
-
-
-
-
-
-	}
 
 
 
---
+++ PrientReceipt.java
@@ -0,0 +1,23 @@
+public class PrientReceipt{
+	
+ public static void main(String[] args){
+    String items1 = "Computer:";
+    String items2 = "Headphones:";
+    int price1 = 980;
+    int price2;
+        price2 = 40;
+
+    int sum = price1 +price2;
+
+     System.out.println("Receipt");
+     System.out.println(items1+price1+" dollars ");
+     System.out.println(items2+price2+" dollars ");
+     System.out.println("_ _ _ _ _ _ _ _ _ ");
+     System.out.println("Total:"+sum+" dollars ");
+
+
+ }
+
+
+
+}
