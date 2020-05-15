8d726d100c2a9ed82e2f074db138f243ad8520d5
NSoltan <65378138+NSoltan@users.noreply.github.com>
2020-05-14 21:00:17 -0500

    Add files via upload

--- PrintReceipt.java
+++ PrintReceipt.java
@@ -1,26 +1,18 @@
 public class PrintReceipt {
 
-	public static void main (String[] args){
+    public static void main(String[] args){
+    	String item1 = "Computer";
+    	String item2 = "Headphones";
+    	int price1 = 980;
+    	int price2 = 40;
+    	int total = price1 + price2;
 
-			String item1 = "980";
-		    String item2 = "40";
+    	System.out.println("   Receipt");
+    	System.out.println(item1+ ": " + price1 + " dollars");
+    	System.out.println(item2+ ": " + price2 + " dollars"); 
+    	System.out.println("-------------------");
+    	System.out.println("Total: " + total + " dollars");
 
-		System.out.println("Computer: "+item1);
-		System.out.println("Headphones: "+item2);
-
-
-		int number1 = 980;
-		int number2;
-		number2 = 40;
-
-		int sum = number1 + number2;
- 		System.out.println("Total:");
-		System.out.println(sum);
-
-	}
-
-
-
-
+    }
 
 }
