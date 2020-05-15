ade02d34ae24385970d721238427381d2966fa9d
raiimzhanova <65378024+raiimzhanova@users.noreply.github.com>
2020-05-15 17:16:08 -0500

    Aiperi's Print Receipt Homework May152020

--- PrintReceipt.java
+++ PrintReceipt.java
@@ -1,18 +1,23 @@
 public class PrintReceipt {
+	
 
-    public static void main(String[] args){
-    	String item1 = "Computer";
-    	String item2 = "Headphones";
-    	int price1 = 980;
-    	int price2 = 40;
-    	int total = price1 + price2;
+	public static void main(String[] args){
+	    int Computer = 980;
+	    int Headphones = 40;
 
-    	System.out.println("   Receipt");
-    	System.out.println(item1+ ": " + price1 + " dollars");
-    	System.out.println(item2+ ": " + price2 + " dollars"); 
-    	System.out.println("-------------------");
-    	System.out.println("Total: " + total + " dollars");
+	    int Total = 1020;
 
-    }
+	    System.out.println("Receipt ");
+
+	    System.out.println("Computer:" + Computer + " dollars");
+	    System.out.println("Headphones:" + Headphones + " dollars");
+
+	    System.out.println("------------------");
+	    
+	    System.out.println("Total:" + Total + " dollars");
+
+
+	}
+
 
 }
