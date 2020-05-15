f98f388cbb8f75b76c0d8562eb8c980ac4bf5389
shabiyeva97 <65377297+shabiyeva97@users.noreply.github.com>
2020-05-14 21:00:33 -0500

    yesterdasys

--- Greetings.java
+++ Greetings.java
@@ -1,6 +1,9 @@
 public class Greetings {
-	public static void main (String[] args){
-		System.out.println("Welcome");
-		System.out.println("Mr. Brown");
-	}
+	
+public static void main (String[] args){
+	System.out.println("Welcome");
+	System.out.println("Mr. Brown");
+	System.out.println("Mr. Smith");
+  }
+
 }
