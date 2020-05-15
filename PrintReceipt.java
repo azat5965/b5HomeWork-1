68a2db5e7c6b0922fb725acfc4317b4152609de1
NSoltan <65378138+NSoltan@users.noreply.github.com>
2020-05-14 21:02:53 -0500

    homework Soltan

--- PrintReceipt.java
+++ PrintReceipt.java
@@ -1,23 +1,18 @@
 public class PrintReceipt {
-	
-	public static void main(String[] args){
-        String a = "Computer";
-	    String b = "Headphones";
-	    int aPrice = 980;
-        int bPrice = 40;
-        int totalPrice = (aPrice+bPrice); 
 
-        System.out.println("    Receipt");
-        System.out.println(a+": "+aPrice+" dollars");
-        System.out.println(b+": "+bPrice+" dollars");
-        System.out.println("-----------------------");
-        System.out.println("Total: "+totalPrice+" dollars");
+    public static void main(String[] args){
+    	String item1 = "Computer";
+    	String item2 = "Headphones";
+    	int price1 = 980;
+    	int price2 = 40;
+    	int total = price1 + price2;
 
+    	System.out.println("   Receipt");
+    	System.out.println(item1+ ": " + price1 + " dollars");
+    	System.out.println(item2+ ": " + price2 + " dollars"); 
+    	System.out.println("-------------------");
+    	System.out.println("Total: " + total + " dollars");
 
-
-
-
-
-	}
+    }
 
 }
