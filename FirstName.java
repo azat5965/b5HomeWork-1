a958acc27409233884e451d1556492d2cc76398f
Nafisa-DevWeb <65377869+Nafisa-DevWeb@users.noreply.github.com>
2020-05-14 21:01:42 -0500

    Add files via upload

--- FirstName.java
+++ FirstName.java
@@ -1,10 +1,7 @@
 public class FirstName {
+	public static void main(String[] args) {
+	String name = "John";
+	System.out.println(name);
 	
-    public static void main(String[] args){
-        String name = "Aiza";
-        System.out.println(name);
-        name = "David";
-        System.out.println(name);
-    }
-
+	}
 }
