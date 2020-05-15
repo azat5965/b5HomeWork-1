b78b6dda3935d097a867aa28f1594f65d26d5f58
raiimzhanova <65378024+raiimzhanova@users.noreply.github.com>
2020-05-15 17:08:38 -0500

    Print Receipt homework May152020

---
+++  PrintReceipt.java
@@ -0,0 +1,16 @@
+piublic class PrintReceipt {
+	
+
+	public static void main(String[] args){
+	    int Computer = 980;
+	    int Headphones = 40;
+
+	    int Total = 1020 dollars;
+
+	    System.out.println("Receipt " + Computer +"dollars" + Headphones + "dollars" + Total);
+
+
+	}
+
+
+}
