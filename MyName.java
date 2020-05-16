14743379bae02e5465d317e3febb69cdb5e4dda4
Nafisa-DevWeb <65377869+Nafisa-DevWeb@users.noreply.github.com>
2020-05-15 23:26:50 -0500

    Nafisa HW#1

--- MyName.java
+++ MyName.java
@@ -1,16 +1,7 @@
 public class MyName {
-	
-	public static void main(String[] args){
-        String firstName = "Aiperi";
-        String lastName = "Raiimzhanova";
-        
-        System.out.println("My first name is " +firstName+ " and last name is "+lastName);
-
-
-         
-
+	public static void main(String[] args) {
+	String firstName = "Nafisa";
+	String lastName = "Luferchyk";
+	 System.out.println("My first name is "+firstName+" and last name is "+lastName);
 	}
-	
-
-
 }
--- PrintReceipt.java
+++ PrintReceipt.java
@@ -1,23 +1,16 @@
 public class PrintReceipt {
-	
+	public static void main(String[] args) {
+		String item1 = "Computer";
+		String item2 = "Headphones";
+		int price1 = 980;
+		int price2 = 40;
 
-	public static void main(String[] args){
-	    int Computer = 980;
-	    int Headphones = 40;
-
-	    int Total = 1020;
-
-	    System.out.println("Receipt ");
-
-	    System.out.println("Computer:" + Computer + " dollars");
-	    System.out.println("Headphones:" + Headphones + " dollars");
-
-	    System.out.println("------------------");
-	    
-	    System.out.println("Total:" + Total + " dollars");
+		System.out.println("     Receipt");
+		System.out.println("Computer: "+price1+" dollars");
+		System.out.println("Headphones: "+price2+" dollars");
+		System.out.println("---------------------");
+		System.out.println("Total: 1020 dollars");
 
 
 	}
-
-
 }
