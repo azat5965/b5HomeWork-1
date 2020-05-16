5575a0f3ed846e7d20a70a9c68a34d17fb123481
LisaFuentes <65377677+LisaFuentes@users.noreply.github.com>
2020-05-16 08:51:35 -0500

    Lisa HW#1

--- MyName.java
+++ MyName.java
@@ -1,7 +1,10 @@
-public class MyName {
-	public static void main(String[] args) {
-	String firstName = "Nafisa";
-	String lastName = "Luferchyk";
-	 System.out.println("My first name is "+firstName+" and last name is "+lastName);
+public class MyName{
+	
+	public static void main(String[] args){
+     String firstName = "Lisa";
+     String lastName = "Fuentes"; 
+
+      System.out.println("My first name is " + firstName+ " and last name is " + lastName);
+
 	}
 }
--- PrintReceipt.java
+++ PrintReceipt.java
@@ -1,14 +1,16 @@
-public class PrintReceipt {
-	public static void main(String[] args) {
-		String item1 = "Computer";
-		String item2 = "Headphones";
-		int total = price1+price2;
+public class PrintReceipt{
+	
+	public static void main(String[] args){
+	String item1 = "Computer";
+	String item2 = "Headphones";
+	int value1 = 980;
+	int value2 = 40;
 
-		System.out.println("     Receipt");
-		System.out.println(item1+":"+price1+" dollars");
-		System.out.println(item2+":"+price2+" dollars");
-		System.out.println("---------------------");
-		System.out.println("Total: "+ total+ " dollars");
-
+	System.out.println("Receipt");
+	System.out.println(item1 + ":" + value1 + " dollars");
+	System.out.println(item2 + ":" + value2 + " dollars");
+	System.out.println("------------------");
+	int total = value1+value2;
+	System.out.println("Total: " + total + " dollars");
 	}
 }
