62407350f36be2b5a27a53700f693d6f3050833a
tim15os <65377248+tim15os@users.noreply.github.com>
2020-05-17 17:50:49 -0500

    HW May172020

--- Discussion.java
+++ Discussion.java
@@ -1,42 +1,38 @@
 import java.util.Scanner;
-public class Discussion{
+public class Discussion {
 	
+public static void main(String[] args){
+	
+ Scanner input = new Scanner(System.in);
+ System.out.println("Hi! What is your name? ");
 
-	public static void main(String[] args){
+ String mName = input.next();
 
-     
-     Scanner input = new Scanner(System.in);
-     
-     System.out.println("Hi! What is your name? ");
+System.out.println("Nice to meet you, "+mName+"!");
 
-     String name = input.next();
+System.out.println("What do you do, "+mName+"?");
 
-     System.out.println("Nice to meet you, "+name );
-     
-     System.out.println("What do you do, "+name+"?" );
+String work = input.next();
 
-     String youDo = input.next();
+System.out.println("So you are a "+work);
 
-     System.out.println("So you are a "+youDo );
+System.out.println("How much is your hourly salary?");
 
-     System.out.println("How much is your hourly salary? " );
+int salary = input.nextInt();
+int sAnnual = (salary*1_920);
 
-     int salary = input.nextInt();
-
-     System.out.println("So you make weekly $"+ (salary*8*5) );
-
-     System.out.println("So you make monthly $"+ (salary*8*5*4) );
-
-     System.out.println("So you make annual $"+ (salary*8*5*4*12) );
+System.out.println("So you make weekly $"+salary*40);
+System.out.println("So you make monthly $"+salary*160);
+System.out.println("So you make annual $"+sAnnual);
 
 
-
-
-     
+// Timur Dzhigkaev 
 
 
 
 
 
-	}
+}
+
+
 }
--- FahrenheitToCelcius.java
+++ FahrenheitToCelcius.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
+public class FahrenheitToCelcius {
+	public static void main(String[] args){
 
-		public class FahrenheitToCelcius {
-			public static void main(String[] args){
-		
-		Scanner input = new Scanner(System.in);
-		System.out.println("Please enter degree in Fahrenheit" );
 
-		float num1 = input.nextInt();	
+Scanner input = new Scanner(System.in);
+ System.out.println("Please enter degree in Fahrenheit: ");
+  int numberOfF = input.nextInt();
+  double numberOfC = (numberOfF - 32)*0.55555555555D;
 
-		System.out.println("It is " +( (num1-32)*5/9 )+    
-			" in Celsius " );
+  System.out.println("It is "+numberOfC+" in Celcius");
+
+// Timur Dzhigkaev
+  
+
+
+
 
 	}
+
+
+
 }
+
