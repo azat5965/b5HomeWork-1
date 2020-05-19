24b18629d1af251723581d4a6644bd9deb74e908
raiimzhanova <65378024+raiimzhanova@users.noreply.github.com>
2020-05-19 15:48:43 -0500

    Homework PrintReceipt

---
+++ Aiperi/PrintReceipt.java
@@ -0,0 +1,23 @@
+public class PrintReceipt {
+	
+
+	public static void main(String[] args){
+	    int Computer = 980;
+	    int Headphones = 40;
+
+	    int Total = 1020;
+
+	    System.out.println("Receipt ");
+
+	    System.out.println("Computer:" + Computer + " dollars");
+	    System.out.println("Headphones:" + Headphones + " dollars");
+
+	    System.out.println("------------------");
+	    
+	    System.out.println("Total:" + Total + " dollars");
+
+
+	}
+
+
+}
