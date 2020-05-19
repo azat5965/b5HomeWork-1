7fa3375a6e4c79ab570ced85f6d82d0c0a81a161
Nargiza6 <65377205+Nargiza6@users.noreply.github.com>
2020-05-19 13:43:56 -0500

    HomeWork class IncrementDecrement.java

---
+++ Nargiza/IncrementDecrement.java
@@ -0,0 +1,16 @@
+package Operators;
+
+public class IncrementDecrement {
+	public static void main(String[] args) {
+		int num1 = 16;
+		int num2 = 22;
+		
+		
+		 
+int total = num1++ + ++num2 + num2-- + --num2 + ++num1 + num1++; 
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(total);
+	}
+
+}
