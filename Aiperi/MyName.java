ab9b2f3baf47d620e6e97c68667963ae33f8dc7c
raiimzhanova <65378024+raiimzhanova@users.noreply.github.com>
2020-05-19 15:47:33 -0500

    My first Homework MyName

---
+++ Aiperi/MyName.java
@@ -0,0 +1,16 @@
+public class MyName {
+	
+	public static void main(String[] args){
+        String firstName = "Aiperi";
+        String lastName = "Raiimzhanova";
+        
+        System.out.println("My first name is " +firstName+ " and last name is "+lastName);
+
+
+         
+
+	}
+	
+
+
+}
