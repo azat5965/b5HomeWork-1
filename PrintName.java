30ecfef89f9b04e0bb155db9324b6852ee55c365
letesteur <65377857+letesteur@users.noreply.github.com>
2020-05-20 17:26:49 -0500

    Homework04

---
+++ PrintName.java
@@ -0,0 +1,39 @@
+package Varibales;
+
+import java.util.Scanner;
+
+public class PrintName {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in); 
+		
+		System.out.println("Please enter your name:");
+		
+		String name = input.next();
+		
+		int lenght = name.length();
+		
+		String userName ="DAVID";
+		System.out.println(name.toUpperCase());
+		System.out.println(userName.charAt(0));
+		System.out.println(userName.charAt(1));
+		System.out.println(userName.charAt(2));
+		System.out.println(userName.charAt(3));
+		System.out.println(userName.charAt(4));
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+	
+	
+		
+	}
+
+}
