7d674b97f0cc4ffbc2f518163383c75af0bad5cc
Sarajdin <65377917+Sarajdin@users.noreply.github.com>
2020-05-20 15:31:55 -0500

    Add files via upload

---
+++ Sarajdin/PrintName.java
@@ -0,0 +1,34 @@
+package Homework04;
+import java.util.Scanner;
+
+public class PrintName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		String name = "Sarajdin";
+		String upperCaseName = name.toUpperCase();
+		
+		System.out.println(upperCaseName.charAt(upperCaseName.length()-8));
+		System.out.println();
+		System.out.println(upperCaseName.charAt(upperCaseName.length()-7));
+		System.out.println();
+		System.out.println(upperCaseName.charAt(upperCaseName.length()-6));
+		System.out.println();
+		System.out.println(upperCaseName.charAt(upperCaseName.length()-5));
+		System.out.println();
+		System.out.println(upperCaseName.charAt(upperCaseName.length()-4));
+		System.out.println();
+		System.out.println(upperCaseName.charAt(upperCaseName.length()-3));
+		System.out.println();
+		System.out.println(upperCaseName.charAt(upperCaseName.length()-2));
+		System.out.println();
+		System.out.println(upperCaseName.charAt(upperCaseName.length()-1));
+		
+		
+		
+
+	}
+
+}
