540f69c70f2231dceb57d208cce7c638c9508f2b
Nargiza6 <65377205+Nargiza6@users.noreply.github.com>
2020-05-20 17:28:15 -0500

    Day6

---
+++ Nargiza/PrintName.java
@@ -0,0 +1,30 @@
+package Variables;
+
+
+
+public class PrintName {
+	public static void main(String[] args) {
+		
+		String name = "Nargiza";
+		
+	 String upperName = name.toUpperCase();
+	    
+	 int letterOfSize = name.length();
+	 
+	 System.out.println(name.toUpperCase());
+	 
+	 System.out.println("Number of letters in your name: "+ letterOfSize);
+	 
+	 System.out.println(upperName.charAt(0));
+	 System.out.println(upperName.charAt(1));
+	 System.out.println(upperName.charAt(2));
+	 System.out.println(upperName.charAt(3));
+	 System.out.println(upperName.charAt(4));
+	 System.out.println(upperName.charAt(5));
+	 System.out.println(upperName.charAt(6));
+	 System.out.println(upperName.charAt(7));
+	
+		
+	}
+
+}
