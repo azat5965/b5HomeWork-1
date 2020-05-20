509c0c1f160a316ecc26dd11416ec96cb0ba40b2
happylife979 <65377757+happylife979@users.noreply.github.com>
2020-05-20 13:54:36 -0400

    Add files via upload

---
+++ PrintMyName.java
@@ -0,0 +1,25 @@
+package Variables;
+
+public class PrintMyName {
+	
+	public static void main(String[] args) {
+		
+		String name = "Aizaada";
+		
+		String upperName = name.toUpperCase();
+		
+		long length = name.length();
+		
+		System.out.println(name);
+		System.out.println(upperName.charAt(0));
+		System.out.println(upperName.charAt(1));
+		System.out.println(upperName.charAt(2));
+		System.out.println(upperName.charAt(3));
+		System.out.println(upperName.charAt(4));
+		System.out.println(upperName.charAt(5));
+		System.out.println(upperName.charAt(6));
+	
+		
+	}
+
+}
