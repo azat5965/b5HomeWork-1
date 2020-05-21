6e32bdc62e7ec1499603551e11274bc6508361f6
Ybayetova <65377168+Ybayetova@users.noreply.github.com>
2020-05-20 20:58:29 -0500

    Add files via upload

---
+++ Yerkenaz/PrintName.java
@@ -0,0 +1,33 @@
+
+
+public class PrintName {
+
+	public static void main (String []args) {
+		
+	 
+		String name = "Yerkenaz";
+		
+		
+		System.out.println (name.toUpperCase()); 
+		
+		String letter1 = "y"; 
+		String letter2 =  "e";
+		String letter3 = "r";
+		String letter4 = "k"; 
+		String letter5 = "e"; 
+		String letter6 = "n"; 
+		String letter7 = "a"; 
+		String letter8 = "z"; 
+		
+		System.out.println (letter1.toUpperCase());
+		System.out.println (letter2.toUpperCase());
+		System.out.println (letter3.toUpperCase());
+		System.out.println (letter4.toUpperCase());
+		System.out.println (letter5.toUpperCase());
+		System.out.println (letter6.toUpperCase());
+		System.out.println (letter7.toUpperCase());
+		System.out.println (letter8.toUpperCase());
+		
+		
+	}
+}
