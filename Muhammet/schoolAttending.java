81acd37a5d94b2090ca3907dab6e322cfad187c9
letesteur <65377857+letesteur@users.noreply.github.com>
2020-05-20 20:38:37 -0500

    hw04

---
+++ Muhammet/schoolAttending.java
@@ -0,0 +1,45 @@
+package Varibales;
+
+import java.util.Scanner;
+
+public class schoolAttending {
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("How many percent you attending the school out of 100? (0-100)");
+		
+		String percent = input.next();
+		
+		System.out.println("Did you finish all your homework? yes/no");
+		String finish = input.next();
+		
+		
+		boolean percentResult = percent.equalsIgnoreCase("yes");
+		boolean finishResult = finish.equalsIgnoreCase("yes");
+		
+		boolean result = percentResult|| finishResult;
+		
+		System.out.println("Have you recapped Java class? yes/no");
+		String recappedJava = input.next();
+		
+		boolean recappedJavaResult = recappedJava.equalsIgnoreCase("no");
+		
+		boolean finalResult = result && recappedJavaResult;
+		
+		System.out.println("Active student"+finalResult);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
