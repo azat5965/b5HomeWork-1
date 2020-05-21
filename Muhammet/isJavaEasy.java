5784e48cfc98a3b75784f58c9f92f8856012fffa
letesteur <65377857+letesteur@users.noreply.github.com>
2020-05-21 16:45:50 -0500

    hw05task1

---
+++ Muhammet/isJavaEasy.java
@@ -0,0 +1,24 @@
+package StringMethods;
+
+public class isJavaEasy {
+	public static void main(String[] args) {
+		
+		String sentence = "Is java easy?";
+		
+		System.out.println(sentence);
+		
+		
+		int indexOfSpace = sentence.indexOf(' ');
+		String word3 = sentence.substring(indexOfSpace);
+		System.out.println("Easy");
+		
+		
+		
+		
+    
+		
+		
+		
+	}
+
+}
