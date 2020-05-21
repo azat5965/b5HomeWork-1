348b4a53c187a18bc5f8f9f621b60b5d8201ad63
halbaytm <65377064+halbaytm@users.noreply.github.com>
2020-05-21 00:59:46 -0700

    IsJavaEasy 5/20/20

---
+++ Hal/IsJavaEasy.java
@@ -0,0 +1,12 @@
+package StringMethods;
+
+public class IsJavaEasy {
+	
+		public static void main(String[] args) {
+			String java = "Is Java Easy?";
+						// Is Java Easy
+			// 012345678901 (2,6) love from 2 to 6
+			System.out.println(java.substring(8,12));
+		}
+
+}
