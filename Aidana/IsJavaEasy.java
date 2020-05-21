427fbf31b15cf818e0d4d04c77641d004d17e37a
AidanaM <65377422+AidanaM@users.noreply.github.com>
2020-05-21 12:47:55 -0400

    Home Work05.21.20
    I try chaining method but it doesn't work , see dreams about easy , hope it will be correct one

---
+++ Aidana/IsJavaEasy.java
@@ -0,0 +1,28 @@
+package StringMethods;
+
+public class IsJavaEasy {
+	public static void main(String[] args) {
+		String sentence = " Is Java easy? ";
+		  sentence = sentence.substring(9,13);
+		  String sentenceWord = sentence.substring(0,1).toUpperCase();
+		 
+		
+		
+		System.out.println(sentenceWord+(sentence.substring(1,4)));
+		
+		
+		
+		
+		
+		
+	
+		
+	
+	}
+
+	
+	}
+
+	
+
+
