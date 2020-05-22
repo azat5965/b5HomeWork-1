4b2745bd82458987501f035cbbca3e62b3662c9c
letesteur <65377857+letesteur@users.noreply.github.com>
2020-05-22 11:21:40 -0500

    Update isJavaEasy.java

--- Muhammet/isJavaEasy.java
+++ Muhammet/isJavaEasy.java
@@ -3,14 +3,11 @@ package StringMethods;
 public class isJavaEasy {
 	public static void main(String[] args) {
 		
-		String sentence = "Is java easy?";
+		String sentence = "Is java easy ?";
+		sentence = sentence.substring(8,13);
+		String sentenceWord = sentence.substring(0,1).toUpperCase();
 		
-		System.out.println(sentence);
-		
-		
-		int indexOfSpace = sentence.indexOf(' ');
-		String word3 = sentence.substring(indexOfSpace);
-		System.out.println("Easy");
+		System.out.println(sentenceWord+(sentence.substring(1,4)));
 		
 		
 		
