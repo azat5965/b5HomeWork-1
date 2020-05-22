df475d79ca01921ef0788058dba07b888fe6eadc
TestMeOutside <65377815+testmeoutside@users.noreply.github.com>
2020-05-21 22:51:10 -0500

    Add files via upload

---
+++ Ridvan/IsJavaEasy.java
@@ -0,0 +1,23 @@
+package Day7;
+
+
+public class IsJavaEasy {
+
+	public static void main(String[] args) {
+		
+		String input = new String();
+		
+		System.out.println("Is Java easy?");
+		
+		String java = new String();
+		System.out.println(" ");
+		System.out.println("Easy ");
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Ridvan/patelSdet.java
@@ -0,0 +1,31 @@
+package Day7;
+
+import java.util.Scanner;
+
+public class patelSdet {
+
+	public static void main(String[] args) {
+	
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Please print your name, position, and company?");
+		
+		String name = input.next();
+		
+		System.out.println("so your name is KUMAR  ");
+        
+		String work = input.next();
+		
+		System.out.println("You work as a  DEVELOPER ");
+		
+        String location = input.next();
+  		
+		System.out.println("Your name is NEW YORK ");
+		
+        String company = input.next();
+		
+		System.out.println("You work for company SALES FORCE ");
+
+	}
+
+}
