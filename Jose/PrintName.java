a5a22d8b535801b4341216e1ff0717036179f310
jmroman59 <65377746+jmroman59@users.noreply.github.com>
2020-05-27 15:15:47 -0500

    Homework 04 Task 1 Print Name

---
+++ Jose/PrintName.java
@@ -0,0 +1,16 @@
+package HomeWorks;
+
+public class PrintName {
+	
+	public static void main(String[] args) {
+		
+		String name = "Jose"; 
+				String nameUpperCase = name.toUpperCase();
+				System.out.println(nameUpperCase.charAt(0));
+				System.out.println(nameUpperCase.charAt(1));
+				System.out.println(nameUpperCase.charAt(2));
+				System.out.println(nameUpperCase.charAt(3));
+		
+	}
+
+}
