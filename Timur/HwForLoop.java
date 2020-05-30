7ec1e20aa690d645c81d4cd111d3b756b6cc9d03
tim15os <65377248+tim15os@users.noreply.github.com>
2020-05-30 18:38:51 -0500

    Hw May 30 2020 ForLoop

---
+++ Timur/HwForLoop.java
@@ -0,0 +1,53 @@
+package forfoop;
+
+public class HwForLoop {
+	
+ public static void main(String[] args) {
+	 
+//****************** 1. -> 3 infinite **************************	 
+//	 for(int i=10; 0<i; i++ ) {
+//		 System.out.println(i);
+//	 }
+	 
+//	 for(int i=10; 0<i; ) {
+//	 System.out.println(i);
+// }
+	 
+//	 for(int i=1; 10>i; i-- ) {
+//	 System.out.println(i);
+// }
+	 
+//***************** 2. from100to0 ****************************************	 
+	
+//	 for(int i=100; i>=0; i-- ) {
+//	 System.out.println(i);
+// } 
+	 
+//***************** 3. multiple from5to25 ****************************************	 	 
+
+//	 int total = 0;	 
+//	 for(int i=5; i<=25 ; ++i) {  
+//	   total += i;
+//	   System.out.println(total); 
+// }  
+	 
+//***************** 4. 2variables sout -34&&35 "Techtorial" ****************************************		
+	 
+	
+//	 for(int i=35, y=-34, x=0;  i>-34 && y<35;  --i,++y,++x) {  
+//	   System.out.println(x+". Techtorial"); 
+// } 
+	  
+//***************** 5. 3variables sout name*15 ****************************************	 
+	 
+     for(int i=1, y=45, x=77, num=0;  i<=15 && y<61 && x>=62;  ++i, y++, --x) {
+    	 num++;
+    	 System.out.println(num+". Timur");
+     }
+	 
+	 
+	 
+	 
+ }
+
+}
