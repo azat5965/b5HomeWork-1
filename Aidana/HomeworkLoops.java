b9fcfcfd742665e39e102d60ce0b155b2f2e0492
MeDan <65377422+MeDa20@users.noreply.github.com>
2020-05-31 01:25:16 -0400

    HomeWork Loops

---
+++ Aidana/HomeworkLoops.java
@@ -0,0 +1,33 @@
+
+public class HomeworkLoops {
+	public static void main(String[] args) {
+		
+		//for(int i = 28 ; i >=0 ;  ) {
+			//System.out.print(i);
+			//System.err.print("Error message");
+		//}
+		//for(int no= 1 ; ; no--){
+		//System.out.print(no);
+	//}
+//for(int i = 1; i >=1 ; i ++) {
+// System.out.println("Help"+i);
+	//}
+		//int total = 0;
+	//for(int count =100; count>=0 ;count++ ) {
+		//System.out.print(", "+total);
+		 // total += count;
+	//}
+		//int multiply = 5;
+	//for(int i=5;i<=25;i++) {
+		// multiply *= i;
+		// System.out.println(multiply);
+		
+	//}
+		 //for(int i =0, y=0; i > -34 && y < 35;i--,y++ ) {
+			// System.out.println(y+" Techtorial "+i);
+		// }
+		for( int i = 10, y = 5, x = 0; x<=i+y ;x++) {
+			System.out.println(x+"Aidana");
+		}
+}
+}
