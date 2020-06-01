d0e386fda144d47ee689ebac274504f9ec7048c4
letesteur <65377857+letesteur@users.noreply.github.com>
2020-06-01 00:28:10 -0500

    hw8

---
+++ Muhammet/ForLoops1.java
@@ -0,0 +1,22 @@
+
+public class ForLoops1 {
+	public static void main(String[] args) {
+		
+		for(int i = 0; i < 10; i++) {
+			System.out.println(i);
+			
+		}
+		
+		for(int i = 0; i <= 10; i++ ) {
+			
+			System.out.println(i);
+		}
+		
+		for (int i = 3; i < 10; i++) {
+			System.out.print(i);
+			
+		}
+	}  
+	
+
+}
