9f79e17a5a67a0cc40828c653ecf1d48b999fbd2
letesteur <65377857+letesteur@users.noreply.github.com>
2020-06-01 00:50:11 -0500

    hw8

---
+++ Muhammet/ForLoops2.java
@@ -0,0 +1,15 @@
+
+public class ForLoops2 {
+	
+	public static void main(String[] args) {
+		
+		int total = 0;
+		for(int i = 100; i >= 0; --i) {
+			System.out.println(total+=i);
+			
+			
+		}
+		
+	}
+
+}
